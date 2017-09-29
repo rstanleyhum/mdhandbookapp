14c1dcb1d4a1e2f256cbe5cf5546f0b2b0a76e6b
R. Stanley Hum <r.stanley.hum@gmail.com>
2017-09-29 11:12:31 -0400

    added tests for RunNumber and ResetlastUpdateTime

--- __tests__/services/appstorage.test.js
+++ __tests__/services/appstorage.test.js
@@ -13,6 +13,68 @@ import * as appstorage from '../../app/services/appstorage';
 
 describe('App Storage Service testing', () => {
 
+    it('GetRunNumber when no number exists', () => {
+        AsyncStorage.getItem = jest.fn( () => { return Promise.resolve(null); });
+        const expectedValue = appstorage.INITIAL_RUN_NUMBER;
+
+        return appstorage.GetRunNumber()
+            .then( (actualValue) => {
+                expect(actualValue).toBe(expectedValue);
+                expect(AsyncStorage.getItem).toBeCalledWith(appstorage.RUN_NUMBER_KEY);
+                AsyncStorage.getItem.mockClear();
+            });
+    });
+
+    it('GetRunNumber when run number exists and is a number', () => {
+        AsyncStorage.getItem = jest.fn( () => { return Promise.resolve("42"); });
+        const expectedValue = 42;
+
+        return appstorage.GetRunNumber()
+            .then( (actualValue) => {
+                expect(actualValue).toBe(expectedValue);
+                expect(AsyncStorage.getItem).toBeCalledWith(appstorage.RUN_NUMBER_KEY);
+            });
+    });
+
+    it('GetRunNumber when and error happens', () => {
+        const err = "error message";
+        AsyncStorage.getItem = jest.fn( () => { return Promise.reject(err); });
+
+        return appstorage.GetRunNumber()
+            .then()
+            .catch( (actualErr) => {
+                expect(actualErr).toBe(err);
+            });
+    });
+
+    it('SaveRunNumber when number is set', () => {
+        const expectedNumber = 5;
+        const expectedNumberString = "5";
+
+        AsyncStorage.setItem = jest.fn( () => { return Promise.resolve(); });
+
+        return appstorage.SaveRunNumber(expectedNumber)
+            .then( () => {
+                expect(AsyncStorage.setItem).toBeCalledWith(appstorage.RUN_NUMBER_KEY, expectedNumberString);
+                AsyncStorage.setItem.mockClear();
+            });
+    });
+
+    it('SaveRunNumber when error occurs', () => {
+        const expectedNumber = 5;
+        const expectedNumberString = "5";
+
+        const err = "error message";
+
+        AsyncStorage.setItem = jest.fn( () => { return Promise.reject(err); });
+
+        return appstorage.SaveRunNumber(expectedNumber)
+            .then( (actualErr) => {
+                expect(actualErr).toBe(err);
+                AsyncStorage.setItem.mockClear();
+            });
+    });
+
     it('GetLastUpdateTime when no time exists', () => {
         AsyncStorage.getItem = jest.fn( () => { return Promise.resolve(null); });
         const expectedValue = appstorage.INITIAL_DEPLOY_TIME;
@@ -46,8 +108,8 @@ describe('App Storage Service testing', () => {
             .then()
             .catch( (actualErr) => {
                 expect(actualErr).toBe(err);
-            })
-    })
+            });
+    });
 
     it('SaveLastUpdateTime when lasttime is set', () => {
         const expectedTimeValue = 5432;
@@ -74,6 +136,16 @@ describe('App Storage Service testing', () => {
             .then()
             .catch( (actualErr) => {
                 expect(actualErr).toBe(err);
+            });
+    });
+
+    it('ResetlastUpdateTime called', () => {
+        appstorage.SaveLastUpdateTime = jest.fn();
+
+        return appstorage.ResetLastUpdateTime()
+            .then( () => {
+                expect(appstorage.SaveLastUpdateTime).toBeCalledWith(appstorage.INITIAL_DEPLOY_TIME);
+
             })
     })
 })
